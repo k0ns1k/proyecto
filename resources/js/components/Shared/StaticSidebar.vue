a6dc86c50862b7edeb1221d76f4a36f4d7ffc857
Juan Carlos Fuenzalida <juancarlosvillarroel96@gmail.com>
2024-08-15 13:01:20 -0400

    UI refactorizada.

---
+++ resources/js/components/Shared/StaticSidebar.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
+        <!-- Sidebar component, swap this element with another sidebar if you like -->
+        <div class="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6 pb-4">
+            <div class="flex h-16 shrink-0 items-center">
+                <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" />
+            </div>
+            <nav class="flex flex-1 flex-col">
+                <ul role="list" class="flex flex-1 flex-col gap-y-7">
+                    <li>
+                        <ul role="list" class="-mx-2 space-y-1">
+                            <li v-for="item in props.links" :key="item.name">
+                                <a :href="item.href" :class="[item.current ? 'bg-gray-800 text-white' : 'text-gray-400 hover:bg-gray-800 hover:text-white', 'group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6']">
+                                    <component :is="item.icon" class="h-6 w-6 shrink-0" aria-hidden="true" />
+                                    {{ item.name }}
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <div class="text-xs font-semibold leading-6 text-gray-400">Your teams</div>
+                        <ul role="list" class="-mx-2 mt-2 space-y-1">
+                            <li v-for="team in props.teams" :key="team.name">
+                                <a :href="team.href" :class="[team.current ? 'bg-gray-800 text-white' : 'text-gray-400 hover:bg-gray-800 hover:text-white', 'group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6']">
+                                    <span class="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-gray-700 bg-gray-800 text-[0.625rem] font-medium text-gray-400 group-hover:text-white">{{ team.initial }}</span>
+                                    <span class="truncate">{{ team.name }}</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="mt-auto">
+                        <a href="#" class="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-400 hover:bg-gray-800 hover:text-white">
+                            <Cog6ToothIcon class="h-6 w-6 shrink-0" aria-hidden="true" />
+                            Settings
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+
+import {Cog6ToothIcon} from "@heroicons/vue/24/outline";
+
+const  props = defineProps(["teams", "links"]);
+
+</script>
